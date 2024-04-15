b0baae3bcef217746291aaf86933db0b63c37cad
maciej790 <maciejmw126@gmail.com>
2024-04-15 22:06:10 +0200

    dzielnie_dokonczyc_i_nauczyc

--- example.s
+++ example.s
@@ -1,6 +1,9 @@
-;zmodyfikowac sprawko o mnozenie + nauczyc sie kodu
-;zaczac dzielenie
-
+;nauczyc sie kodu dzielenie
+;pomeczyc moze jeszcze troche to dzielenie - hex
+;zrobic sprawko z dzielenia
+;zlepic calosc
+;nauczuc sie calosci
+;kod cpp (przerobic) + lista krokow
 
 SYS_EXIT  equ 1
 SYS_READ  equ 3 ;czytaj
@@ -27,149 +30,80 @@ _start:
 
     mov eax, SYS_READ  
     mov ebx, STDIN  
-    mov ecx, operacja
-    mov edx, 2
-    int 0x80 
-
-    mov eax, SYS_READ  
-    mov ebx, STDIN  
     mov ecx, liczba2
     mov edx, 100
     int 0x80
-
-    mov edx, [operacja]
-
-    mov al, [ecx] ;operacja
-    cmp al, '+' ;jesli plus to dodawanie
-    je funkcja_dodawanie_petla
-    cmp al, '-' ;jesli minus to odejmowanie
-    je funkcja_odejmowanie_petla
-    cmp al, '*' ;jesli gwiazdka to razy
-    je funkcja_mnozenie_petla
 
     mov esi, liczba1
     mov edi, liczba2  
     mov ecx, 100
 
 ;funkcje
-funkcja_dodawanie_petla:
-    mov al, [esi + ecx - 1]  
-    sub al, '0'     
-    add al, [edi + ecx - 1]   
-    sub al, '0'   
+funkcja_dzielenie:
+    ; Inicjalizacja licznika
+    xor ecx, ecx
     
-    cmp al, 15 ;sprawdzenie czy wynik nie przekroczyl 15 - maksymalny wynik dodawania w szesnastkowym
-    jbe funkcja_zapisz_wynik_dodawanie  ;jesli nie    
+    ; Przygotowanie do pętli dzielenia
+    mov esi, liczba1 ; Pierwsza liczba
+    mov edi, liczba2 ; Druga liczba
 
-    sub al, 16
-    add al, 7h
+    sub esi, '0'
+    sub edi, '0'
+
+    ; Inicjalizacja wyniku
+    mov ebx, wynik
+    mov byte [ebx], '0' ; Ustaw pierwszą cyfrę wyniku na zero
+    inc ebx
+
+dzielenie_nastepna_cyfra:
+    ; Przygotowanie do sprawdzenia, czy kolejna cyfra może być dodana do wyniku
+    mov eax, dword [esi + ecx] ; Wczytaj kolejne 4 bajty z pierwszej liczby
+
+    ; Sprawdź, czy jest możliwe dodanie kolejnej cyfry do wyniku
+    cmp eax, 0
+    je dzielenie_wynik
+
+    ; Dodaj kolejną cyfrę do wyniku
+    add ebx, ecx
+    mov dword [ebx], eax
+
+    ; Przygotowanie do przesunięcia reszty
+    mov edx, 0
+
+dzielenie_podzial:
+    ; Sprawdź, czy reszta jest większa lub równa drugiej liczbie
+    cmp edx, dword [edi]
+    jb dzielenie_podzial_koniec
+
+    ; Oblicz nową cyfrę wyniku i resztę
+    inc byte [ebx]
+    sub edx, dword [edi]
+
+    ; Powtórz, dopóki reszta jest większa lub równa drugiej liczbie
+    jmp dzielenie_podzial
+
+dzielenie_podzial_koniec:
+    ; Przygotuj się do kolejnej iteracji
+    add ecx, 4 ; Przesunięcie do kolejnej cyfry
+    inc ebx ; Przesunięcie do kolejnej cyfry wyniku
+    xor edx, edx ; Wyzeruj resztę
+
+    ; Przygotowanie do sprawdzenia, czy jest możliwe dodanie kolejnej cyfry do wyniku
+    cmp ecx, 100 ; Zakładając, że liczba1 ma maksymalnie 100 cyfr
+    jl dzielenie_nastepna_cyfra
+
+dzielenie_wynik:
+    ; Wyświetlenie wyniku
+    mov al, [wynik]
     add al, '0'
-    inc byte [esi + ecx - 1] ;przenieseinie +1 
-
-funkcja_zapisz_wynik_dodawanie:
-    add al, 7h      ;konwersja na hex prawdopodobnie zle konwertuje do poprawy
-    add al, '0'     ;ascii
-    mov [wynik + ecx - 1], al
-    dec ecx ; -1
-    jnz funkcja_dodawanie_petla ;dopoki !=0
-
-    mov edx, 101     
-    mov ecx, wynik 
-    mov ebx, STDOUT     
-    mov eax, SYS_WRITE     
-    int 0x80
-
-    call koniec    
-
-funkcja_odejmowanie_petla:
-    mov al, [esi + ecx - 1]   ;zapisz najmlodzy bit - przesuniecie
-    sub al, '0'               
-    mov bl, [edi + ecx - 1]   
-    sub bl, '0'              
-
-    cmp al, bl                
-    jl pozyczka  ;jesli cyfra 1 mneijsza - pozuczka    
-
-    sub al, bl 
-    add bl, 7h
-    add al, '0'               
-    jmp funkcja_zapisz_wynik_odejmowanie        
-
-pozyczka:
-    add al, 16            
-    sub al, bl                
-    add al, 7h 
-    add al, '0'
-    dec byte [esi + ecx - 1] ;pozyczam z nastepnej pozycji -1                   
-
-funkcja_zapisz_wynik_odejmowanie:
-    add al, 7h
-    add al, '0'
-    mov [wynik + ecx - 1], al  ; Zapisz wynik
-    dec ecx                     ; Przejdź do poprzedniej cyfry
-    jnz funkcja_odejmowanie_petla           ; Jeśli nie, kontynuuj odejmowanie
-
-    mov edx, 101   
-    mov ecx, wynik  
+    add al, 7h ; Dodaj 7h, aby przekształcić cyfrę w systemie szesnastkowym
+    mov edx, 101 ; Liczba cyfr do wyświetlenia
+    sub edx, wynik ; Odejmij wskaźnik początkowy od końcowego, aby uzyskać długość
+    mov ecx, wynik
     mov ebx, STDOUT   
     mov eax, SYS_WRITE 
     int 0x80     
 
-    call koniec 
-
-funkcja_mnozenie_petla:
-    mov ecx, 0
-    mov edi, liczba2
-petla_liczba2:
-    mov bl, [edi + ecx - 1]
-    sub bl, '0'
-    mov esi, liczba1
-    mov edx, 0  ; Przeniesienie
-petla_liczba1:
-    ; cyfra liczby1
-    mov al, [esi + edx - 1]
-    sub al, '0'
-    mul bl ;tutaj asm nasm akceptuje mnozenie tylko w rejestrze bl a wynik przetrzymywany jest w ax
-    add al, 7h
-    add al, '0'
-    add al, [wynik + ecx + edx] ;dodanie iloczynu czesciowego na odpowiendia pozucje
-    cmp al, 15 
-    jbe bez_przeniesienia
-
-    ; przeniesienie
-    sub al, 16
-    inc byte [wynik + ecx + edx + 1]
-bez_przeniesienia:
-    ;zapisz wynik
-    add al, 7h
-    add al, '0'
-    mov [wynik + ecx + edx], al
-
-    ; liczba1++
-    inc edx
-    cmp byte [esi + edx - 1], 0  ; czy koniec liczby1
-    jne petla_liczba1
-
-    ; liczba2++
-    inc ecx
-    cmp byte [edi + ecx - 1], 0  ;; czy koniec liczby2
-    jne petla_liczba2
-
-
-funkcja_zapisz_wynik_mnozenie:
-    add al, '0'
-    add al, 7h
-    mov [wynik + ecx - 1], al
-    mov edx, 101   
-    mov ecx, wynik  
-    mov ebx, STDOUT   
-    mov eax, SYS_WRITE 
-    int 0x80     
-
-    call koniec        
-  
-
-;KONIEC
-koniec:
+    ; Wyjście
     mov eax, SYS_EXIT  
-    int 0x80           
+    int 0x80
